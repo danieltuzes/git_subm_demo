6c4e94888e2d9ddbc1d0b5e398544eb0088beab0
from Manu <danieltuzes@gmail.com>
2023-01-10 14:28:37 -0500

    Update print_f.py

--- my_helper/print_f.py
+++ my_helper/print_f.py
@@ -2,9 +2,9 @@
 
 import numpy
 
-__version__="1.0.3"
+__version__="1.0.4"
 
-def my_pr_f():
+def my_print_f():
     """Prints versions."""
     print("You have my_helper version", __version__)
     print("You have numpy version", numpy.__version__)
