a118789e64a04eb3b1e1dc40f8d801075c62859e
from Manu <danieltuzes@gmail.com>
2023-09-13 14:24:08 -0400

    replace numpy with pandas in the version printout

--- my_helper/print_f.py
+++ my_helper/print_f.py
@@ -1,13 +1,14 @@
 """Minimal repo with dependency on numpy."""
 
 import sys
-import numpy
+import pandas
 
-__version__="1.0.5"
+__version__ = "1.0.6"
+
 
 def my_print_f():
     """Print diagnostics."""
     print(f"My helper's print file is at {__file__}, version {__version__}")
-    print(f"numpy version {numpy.__version__}")
+    print(f"pandas version {pandas.__version__}")
     print(f"sys.argv = {sys.argv}")
     print(f"sys.path = {sys.path}")
